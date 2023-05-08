397a36d694452ce7c1d2dfa05aef2d709be7f3f1
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2023-05-08 20:15:49 +0200

    swith to mongodb

--- compose/order-service-db/mongo-init.js
+++ compose/order-service-db/mongo-init.js
@@ -5,24 +5,12 @@ db.createUser(
         roles: [
             {
                 role: "readWrite",
-                db: "addressdb"
+                db: "OrderDB"
             }
         ]
     }
 );
 
-db = new Mongo().getDB('addressdb');
+db = new Mongo().getDB('OrderDB');
 
-db.createCollection('Address');
-
-db.Address.insertMany([
-    {
-        "_id": ObjectId("644874b3777555522fd294ca"),
-        "city": "Emblem",
-        "country": "BE",
-        "hash": "c49a20095a4f5ac6f83a3c0ff33854327b1cf3c2c5b39aa6f934c1fe53c1dd61",
-        "state": "Antwerpen",
-        "street": "Kloosterstraat 27A",
-        "zip": "2520"
-    }
-]);
+db.createCollection('TacoOrders');
